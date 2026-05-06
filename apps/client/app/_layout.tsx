89c957ee3cc2f03c24202df4d0182d0afae3833b
cjhih456 <cjhih43@gmail.com>
2026-05-06 23:42:17 +0900

    fix: design

--- apps/client/app/_layout.tsx
+++ apps/client/app/_layout.tsx
@@ -1,13 +1,12 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
 import '../global.css';
+import 'react-native-reanimated';
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { AppErrorBoundary } from '@kakamu/ui';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -47,14 +46,13 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <AppErrorBoundary>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
-    </ThemeProvider>
+    </AppErrorBoundary>
   );
 }
