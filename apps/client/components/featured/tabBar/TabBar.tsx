92ee9d871d1a19e4f701ef62e444835f42cfb6ff
cjhih456 <cjhih43@gmail.com>
2026-05-08 23:25:32 +0900

    fix: ui design

--- apps/client/components/featured/tabBar/TabBar.tsx
+++ apps/client/components/featured/tabBar/TabBar.tsx
@@ -9,7 +9,7 @@ export function TabBar({ children }: TabBarProps) {
   return (
     <TextClassContext.Provider value="text-card-foreground">
       <View
-        className="bg-card border border-border flex-row items-stretch gap-0.5 rounded-full h-[62px] mx-[21px] mt-3 mb-[21px] p-1"
+        className="bg-card border border-border flex-row items-stretch gap-0.5 rounded-full h-15.5 mx-5.5 mt-3 mb-5.5 p-1"
         accessibilityRole="tablist"
       >
         {children}
