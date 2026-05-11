a3e153666db9a34ed0ad9756e624a7501b3dc09a
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2026-05-11 20:29:26 +0900

    Revert "Default package"

--- apps/client/components/featured/intro/StyledLinearGradient.tsx
+++
@@ -1,6 +0,0 @@
-import { LinearGradient } from 'expo-linear-gradient';
-import { styled } from 'nativewind';
-
-export const StyledLinearGradient = styled(LinearGradient, {
-  className: 'style',
-});
