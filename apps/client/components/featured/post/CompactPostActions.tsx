7102c0bde1e62cc2b7d481a520ab91f91dea62d9
cjhih456 <cjhih43@gmail.com>
2026-05-30 19:44:47 +0900

    fix: post action design

--- apps/client/components/featured/post/CompactPostActions.tsx
+++ apps/client/components/featured/post/CompactPostActions.tsx
@@ -2,19 +2,18 @@ import { View } from "react-native";
 import { PostItem } from "@kakamu/types";
 import { Icon, Text, Button, TextClassProvider, DropdownMenu, DropdownMenuItem, DropdownMenuContent, DropdownMenuTrigger } from "@kakamu/ui";
 import { MessageCircle, Heart, Bookmark, Ellipsis } from "lucide-react-native";
-import { useCallback } from "react";
 import { ConditionalRender } from "@/components/utils/ConditionalRender";
 
 
 type CompactPostActionsProps = {
   post: PostItem;
   isOwner?: boolean;
-  onToggleLike?: (id: number) => void;
-  onComment?: (id: number) => void;
-  onToggleBookmark?: (id: number) => void;
-  onDelete?: (id: number) => void;
-  onModify?: (id: number) => void
-  onReport?: (id: number) => void
+  onToggleLike?: () => void;
+  onComment?: () => void;
+  onToggleBookmark?: () => void;
+  onDelete?: () => void;
+  onModify?: () => void
+  onReport?: () => void
 };
 
 export function CompactPostActions({
@@ -27,40 +26,22 @@ export function CompactPostActions({
   onModify,
   onReport
 }: CompactPostActionsProps) {
-  const handleToggleLike = useCallback(() => {
-    onToggleLike?.(post.id);
-  }, [post.id, onToggleLike]);
-  const handleComment = useCallback(() => {
-    onComment?.(post.id);
-  }, [post.id, onComment]);
-  const handleToggleBookmark = useCallback(() => {
-    onToggleBookmark?.(post.id);
-  }, [post.id, onToggleBookmark]);
-  const handleDelete = useCallback(() => {
-    onDelete?.(post.id);
-  }, [post.id, onDelete]);
-  const handleModify = useCallback(() => {
-    onModify?.(post.id);
-  }, [post.id, onModify]);
-  const handleReport = useCallback(() => {
-    onReport?.(post.id);
-  }, [post.id, onReport]);
   return (
     <View className="flex-row items-center gap-4">
       <TextClassProvider value="text-muted-foreground">
-        <Button size="text" variant={'ghost'} onPress={handleComment}>
+        <Button size="text" variant={'ghost'} onPress={onComment}>
           <View className="flex-row items-center gap-1">
             <Icon as={MessageCircle} size={16} />
             <Text className="text-xs">{post.comment_count}</Text>
           </View>
         </Button>
-        <Button size="text" variant={'ghost'} onPress={handleToggleLike}>
+        <Button size="text" variant={'ghost'} onPress={onToggleLike}>
           <View className="flex-row items-center gap-1">
             <Icon as={Heart} size={16} />
             <Text className="text-xs">{post.like_count}</Text>
           </View>
         </Button>
-        <Button size="text" variant={'ghost'} onPress={handleToggleBookmark}>
+        <Button size="text" variant={'ghost'} onPress={onToggleBookmark}>
           <Icon as={Bookmark} size={15} />
         </Button>
         <DropdownMenu>
@@ -75,17 +56,17 @@ export function CompactPostActions({
               render={{
                 true: (
                   <>
-                    <DropdownMenuItem onPress={handleModify}>
+                    <DropdownMenuItem onPress={onModify}>
                       <Text>Modify</Text>
                     </DropdownMenuItem>
-                    <DropdownMenuItem onPress={handleDelete}>
+                    <DropdownMenuItem onPress={onDelete}>
                       <Text>Delete</Text>
                     </DropdownMenuItem>
                   </>
                 ),
                 false: (
                   <>
-                    <DropdownMenuItem onPress={handleReport}>
+                    <DropdownMenuItem onPress={onReport}>
                       <Text>Report</Text>
                     </DropdownMenuItem>
                   </>
